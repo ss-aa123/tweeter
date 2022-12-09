bc869f1720cc13a06fb3024c3bbc047d2240deb3
ss-aa123 <neermas1@hotmail.com>
2022-12-09 16:49:17 -0500

    commit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweets = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+
+  $('#all-tweets').empty();
+  for (let tweet of tweets) {
+    $('#all-tweets').append.createTweetElement(tweet)
+  }
+}
+
+const createTweetElement = function(tweet) {
+  let $tweet = $(`
+    <article class="tweet">
+        <header>
+          <div class="icon-and-name">
+            <img src="${tweet.user.avatars}"></img> 
+            <h4 class="userName">${tweet.user.name}</h4>
+          </div>
+          <div class="handle">
+            <h4>${tweet.user.handle}</h4>
+          </div>
+        </header>
+        <p>${tweet.content.text}</p>
+        <footer>
+          <span>${timeago.format(tweet.created_at)}</span>
+          <div class="icons">
+            <i class="fa-solid fa-flag"></i>              
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-sharp fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>`
+      );
+
+  return $tweet;
+};
+
+renderTweets(data);
+
